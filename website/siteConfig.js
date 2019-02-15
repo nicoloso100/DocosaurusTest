a1dda231f73c55ddeb87c9bfe9b4b28e86fba2a9
Nicolas <Nicolas  Angarita Ortiz>
2019-02-15 16:29:53 -0500

    Some minor changes

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -5,100 +5,51 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-// See https://docusaurus.io/docs/site-config for all the possible
-// site configuration options.
-
-// List of projects/orgs using your project for the users page.
-const users = [
-  {
-    caption: 'User1',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-    image: '/img/docusaurus.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
-];
-
 const siteConfig = {
-  title: 'Documentacion de Facturación Electrónica', // Title for your website.
-  tagline: 'Página de FE',
-  url: 'http://localhost/ss/', // Your website URL
-  baseUrl: 'http://localhost/ss/', // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+  title: 'Documentacion FE',
+  url: 'http://localhost/docusaurus/',
+  baseUrl: '/docusaurus/',
 
-  // Used for publishing and more
+  
   projectName: 'DocumentacionFE',
   organizationName: 'SincoSoft',
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
 
-  // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     {doc: 'doc11', label: 'Documentación Cliente'},
     {doc: 'doc21', label: 'Documentación Desarrollo'},
   ],
 
-  // If you have users set above, you add it here:
-  users,
+  headerIcon: 'img/archive.png',
+  footerIcon: 'img/archive.png',
+  favicon: 'img/archive.png',
 
-  /* path to images for header/footer */
-  headerIcon: 'img/logo.png',
-  footerIcon: 'img/logo.png',
-  favicon: 'img/favicon.png',
-
-  /* Colors for website */
   colors: {
-    primaryColor: '#B8C0E5',
-    secondaryColor: '#000000',
+    primaryColor: '#140E39',
+    secondaryColor: '#FF0000',
   },
 
-  /* Custom fonts for website */
-  /*
-  fonts: {
-    myFont: [
-      "Times New Roman",
-      "Serif"
-    ],
-    myOtherFont: [
-      "-apple-system",
-      "system-ui"
-    ]
-  },
-  */
-
-  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
   copyright: `Copyright © ${new Date().getFullYear()} SincoSoft`,
 
   highlight: {
-    // Highlight.js theme to use for syntax highlighting in code blocks.
     theme: 'default',
   },
 
-  // Add custom scripts here that would be placed in <script> tags.
   scripts: ['https://buttons.github.io/buttons.js'],
 
-  // On page navigation for the current documentation page.
   onPageNav: 'separate',
-  // No .html extensions for paths.
+  
   cleanUrl: true,
 
-  // Open Graph and Twitter card images.
   ogImage: 'img/logo.png',
   twitterImage: 'img/logo.png',
   docsSideNavCollapsible: true,
-  // Show documentation's last contributor's name.
-  // enableUpdateBy: true,
 
-  // Show documentation's last update time.
-  // enableUpdateTime: true,
 
-  // You may provide arbitrary config keys to be used as needed by your
-  // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/test-site',
+  scrollToTop: true,
+  scrollToTopOptions: {
+    backgroundColor: '#140E39',
+  },
+
 };
 
 module.exports = siteConfig;
